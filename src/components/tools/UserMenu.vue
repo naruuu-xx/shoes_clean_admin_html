34e5d59f56ad3374259e242bd07acb6576cf2ad8
SCSP-WZY <782105758@qq.com>
2022-09-28 10:34:12 +0800

    修改前端的文字描述

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -38,18 +38,18 @@
         <span v-if="isDesktop()">欢迎您，{{ nickname() }}</span>
       </span>
       <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
-        <a-menu-item key="0">
-          <router-link :to="{ name: 'account-center' }">
-            <a-icon type="user"/>
-            <span>个人中心</span>
-          </router-link>
-        </a-menu-item>
-        <a-menu-item key="1">
-          <router-link :to="{ name: 'account-settings-base' }">
-            <a-icon type="setting"/>
-            <span>账户设置</span>
-          </router-link>
-        </a-menu-item>
+<!--        <a-menu-item key="0">-->
+<!--          <router-link :to="{ name: 'account-center' }">-->
+<!--            <a-icon type="user"/>-->
+<!--            <span>个人中心</span>-->
+<!--          </router-link>-->
+<!--        </a-menu-item>-->
+<!--        <a-menu-item key="1">-->
+<!--          <router-link :to="{ name: 'account-settings-base' }">-->
+<!--            <a-icon type="setting"/>-->
+<!--            <span>账户设置</span>-->
+<!--          </router-link>-->
+<!--        </a-menu-item>-->
         <a-menu-item key="3"  @click="systemSetting">
            <a-icon type="tool"/>
            <span>系统设置</span>
