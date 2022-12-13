568411cf5e89c2270b77cd1492499eac3d9ab255
naruuu-xx <parasitenruuu@gmail.com>
2022-12-13 18:39:18 +0800

    新增推广详情页面。

---
+++ src/views/shoesClean/ShoePromotionStatistics/ShoePromotionStatisticsInfoList.vue
@@ -0,0 +1,195 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline" @keyup.enter.native="searchQuery">
+        <a-row :gutter="24">
+          <a-col :xl="4" :lg="7" :md="8" :sm="24">
+            <a-form-item label=" 推广渠道">
+              <a-input placeholder="请输入推广渠道" v-model="queryParam.channel" :allowClear="true"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <!-- 查询区域-END -->
+
+    <!--    &lt;!&ndash; 操作按钮区域 &ndash;&gt;-->
+    <!--    <div class="table-operator">-->
+    <!--      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>-->
+    <!--      <a-button type="primary" icon="download" @click="handleExportXls('shoe_promotion_statistics')">导出</a-button>-->
+    <!--      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">-->
+    <!--        <a-button type="primary" icon="import">导入</a-button>-->
+    <!--      </a-upload>-->
+    <!--      &lt;!&ndash; 高级查询区域 &ndash;&gt;-->
+    <!--      <j-super-query :fieldList="superFieldList" ref="superQueryModal" @handleSuperQuery="handleSuperQuery"></j-super-query>-->
+    <!--      <a-dropdown v-if="selectedRowKeys.length > 0">-->
+    <!--        <a-menu slot="overlay">-->
+    <!--          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>-->
+    <!--        </a-menu>-->
+    <!--        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>-->
+    <!--      </a-dropdown>-->
+    <!--    </div>-->
+
+    <!-- table区域-begin -->
+    <div>
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+      </div>
+
+      <a-table
+        ref="table"
+        size="middle"
+        :scroll="{x:true}"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        class="j-table-force-nowrap"
+        @change="handleTableChange">
+
+        <template slot="htmlSlot" slot-scope="text">
+          <div v-html="text"></div>
+        </template>
+        <template slot="imgSlot" slot-scope="text,record">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
+          <img v-else :src="getImgView(text)" :preview="record.id" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+        </template>
+        <template slot="fileSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
+          <a-button
+            v-else
+            :ghost="true"
+            type="primary"
+            icon="download"
+            size="small"
+            @click="downloadFile(text)">
+            下载
+          </a-button>
+        </template>
+
+        <span slot="action" slot-scope="text, record">
+          <a  @click="jumpToDetail(record)">详情</a>
+          <!--          <a-divider type="vertical" />-->
+          <!--          <a-dropdown>-->
+          <!--            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>-->
+          <!--            <a-menu slot="overlay">-->
+          <!--              <a-menu-item>-->
+          <!--                <a @click="handleDetail(record)">详情</a>-->
+          <!--              </a-menu-item>-->
+          <!--              <a-menu-item>-->
+          <!--                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">-->
+          <!--                  <a>删除</a>-->
+          <!--                </a-popconfirm>-->
+          <!--              </a-menu-item>-->
+          <!--            </a-menu>-->
+          <!--          </a-dropdown>-->
+        </span>
+
+      </a-table>
+    </div>
+
+    <shoe-promotion-statistics-modal ref="modalForm" @ok="modalFormOk"></shoe-promotion-statistics-modal>
+  </a-card>
+</template>
+
+<script>
+
+import '@/assets/less/TableExpand.less'
+import { mixinDevice } from '@/utils/mixin'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import ShoePromotionStatisticsModal from './modules/ShoePromotionStatisticsModal'
+
+export default {
+  name: 'ShoePromotionStatisticsList',
+  mixins:[JeecgListMixin, mixinDevice],
+  components: {
+    ShoePromotionStatisticsModal
+  },
+  data () {
+    return {
+      description: 'shoe_promotion_statistics管理页面',
+      // 表头
+      columns: [
+        {
+          title:'推广渠道',
+          align:"center",
+          dataIndex: 'channel'
+        },
+        {
+          title:'总数',
+          align:"center",
+          dataIndex: 'count'
+        },
+        {
+          title: '今日',
+          align:'center',
+          dataIndex:'todayCount'
+        },
+        {
+          title: '本月',
+          align:'center',
+          dataIndex:'monthCount'
+        },
+        {
+          title: '本月',
+          align:'center',
+          dataIndex:'yearCount'
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          align:"center",
+          fixed:"right",
+          width:147,
+          scopedSlots: { customRender: 'action' }
+        }
+      ],
+      url: {
+        list: "/shoes/shoePromotionStatistics/list",
+        delete: "/shoes/shoePromotionStatistics/delete",
+        deleteBatch: "/shoes/shoePromotionStatistics/deleteBatch",
+        exportXlsUrl: "/shoes/shoePromotionStatistics/exportXls",
+        importExcelUrl: "shoes/shoePromotionStatistics/importExcel",
+      },
+      dictOptions:{},
+      superFieldList:[],
+    }
+  },
+  created() {
+    this.getSuperFieldList();
+  },
+  computed: {
+    importExcelUrl: function(){
+      return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+    },
+  },
+  methods: {
+    initDictConfig(){
+    },
+    getSuperFieldList(){
+      let fieldList=[];
+      fieldList.push({type:'int',value:'promotionId',text:'推广渠道id'})
+      fieldList.push({type:'int',value:'promotionNum',text:'推广人数'})
+      fieldList.push({type:'date',value:'date',text:'日期'})
+      this.superFieldList = fieldList
+    },
+    jumpToDetail(record){
+
+    }
+  }
+}
+</script>
+<style scoped>
+@import '~@assets/less/common.less';
+</style>
