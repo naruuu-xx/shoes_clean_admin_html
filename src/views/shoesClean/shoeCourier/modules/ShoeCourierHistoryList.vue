59ed91e24a9043d1fa83fc223cc31ea3230597ea
USER-20220902BJ\Administrator <774401914@qq.com>
2023-01-05 11:17:08 +0800

    新增配送员订单字段和搜索条件

--- src/views/shoesClean/shoeCourier/modules/ShoeCourierHistoryList.vue
+++ src/views/shoesClean/shoeCourier/modules/ShoeCourierHistoryList.vue
@@ -1,9 +1,9 @@
 <template>
-  <a-modal v-model:visible="courierHistoryVisible" title="配送员排班" :footer="null" width="1200px">
+  <a-modal v-model:visible="courierHistoryVisible" title="配送员订单" :footer="null" width="1200px">
         <div class="table-page-search-wrapper">
           <a-form layout="inline" @keyup.enter.native="searchQuery">
             <a-row :gutter="24">
-              <a-col :xl="4" :lg="7" :md="8" :sm="24">
+              <a-col :xl="3" :lg="7" :md="8" :sm="24">
                 <a-form-item label=" 姓名:">
                   {{ name }}
                 </a-form-item>
@@ -24,14 +24,43 @@
 <!--                  <a-input placeholder="请输入 结束时间" v-model="queryParam.endTime"></a-input>-->
 <!--                </a-form-item>-->
 <!--              </a-col>-->
+              <a-col :xl="6" :lg="7" :md="8" :sm="24">
+                <a-form-item label="订单状态">
 
+                  <a-select v-model="queryParam.status" style="width: 180px">
+                    <a-select-option v-for="item in statusOptionList" :value="item.value" :key="item.value">
+                      {{ item.name }}
+                    </a-select-option>
+                  </a-select>
+                </a-form-item>
+              </a-col>
+<!--              <a-col :xl="7" :lg="7" :md="8" :sm="24">-->
+<!--                <a-form-item label="接单时间">-->
+<!--                  <a-range-picker @change="onChange" />-->
+<!--                </a-form-item>-->
+<!--              </a-col>-->
+<!--              <a-col :xl="7" :lg="7" :md="8" :sm="24">-->
+<!--                <a-form-item label="开始时间">-->
+<!--                  <a-range-picker @change="onChange2" />-->
+<!--                </a-form-item>-->
+<!--              </a-col>-->
               <a-col :xl="5" :lg="7" :md="8" :sm="24">
-                <a-form-item label="开始时间">
+                <a-form-item label="接单时间">
+                  <j-date placeholder="请选择时间" v-model="queryParam.createStartTime"></j-date>
+                </a-form-item>
+              </a-col>
+              <a-col :xl="5" :lg="7" :md="8" :sm="24">
+                <a-form-item label="接单截止时间">
+                  <j-date placeholder="请选择时间" v-model="queryParam.createEndTime"></j-date>
+                </a-form-item>
+              </a-col>
+              <a-col :xl="5" :lg="7" :md="8" :sm="24">
+                <a-form-item label="完成时间">
                   <j-date placeholder="请选择时间" v-model="queryParam.startTime"></j-date>
                 </a-form-item>
               </a-col>
               <a-col :xl="5" :lg="7" :md="8" :sm="24">
-                <a-form-item label="结束时间">
+                <a-form-item label="完成截止时间">
                   <j-date placeholder="请选择时间" v-model="queryParam.endTime"></j-date>
                 </a-form-item>
               </a-col>
@@ -82,6 +111,7 @@ import '@/assets/less/TableExpand.less'
 import { mixinDevice } from '@/utils/mixin'
 import { JeecgListMixin } from '@/mixins/JeecgListMixin'
 import {deleteAction, getAction} from "../../../../api/manage";
+import {filterDictTextByCache} from "@comp/dict/JDictSelectUtil";
 
 
 export default {
@@ -97,6 +127,7 @@ export default {
     return {
       description: 'shoe_schedule管理页面',
       queryParam:{},
+      statusOptionList: [{"value":"0", "name":"未完成"},{"value":"2", "name":"已完成"}],
       // 表头
       columns: [
         {
@@ -107,7 +138,11 @@ export default {
         {
           title:' 配送费(元)',
           align:"center",
-          dataIndex: 'commission'
+          dataIndex: 'commission',
+          customRender: (text) => {
+            let commission = (text/100);
+            return commission;
+          },
         },
         {
           title:' 用户姓名',
@@ -118,6 +153,19 @@ export default {
           title:' 用户手机号',
           align:"center",
           dataIndex: 'phone'
+        },
+        {
+          title:' 订单状态',
+          align:"center",
+          dataIndex: 'status',
+          customRender: (text) => {
+            return filterDictTextByCache('courier_order_status', text);
+          }
+        },
+        {
+          title:' 接单时间',
+          align:"center",
+          dataIndex: 'createTime'
         },
         {
           title:' 完成时间',
@@ -157,6 +205,9 @@ export default {
         "noc":this.queryParam.noc,
         "putStartTime":this.queryParam.startTime,
         "putEndTime":this.queryParam.endTime,
+        "createStartTime":this.queryParam.createStartTime,
+        "createEndTime":this.queryParam.createEndTime,
+        "status":this.queryParam.status,
       }
       //getAction(this.url.list+"?id="+this.courierId+"&noc="+this.queryParam.noc+"&putStartTime="+this.queryParam.startTime+"&putEndTime="+this.queryParam.endTime).then((res) => {
         getAction(this.url.list,data).then((res) => {
