65f04f71a96f60649fc521e514fedd56cf11ba2d
DESKTOP-4EDG6UN\UU <wang_guanxing2022@163.com>
2023-01-12 17:44:15 +0800

    新机柜数量

--- src/views/shoesClean/ShoeLockerNew/modules/shoeLockerGridModal.vue
+++ src/views/shoesClean/ShoeLockerNew/modules/shoeLockerGridModal.vue
@@ -1,63 +1,25 @@
 <template>
-
-    <j-modal
-      :title="title"
-      :width="width"
-      :visible="visible"
-      switchFullscreen
-      @cancel="handleCancel"
-      cancelText="关闭"
-      :footer="null">
-      <a-spin :spinning="confirmLoading">
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7">
-  <!--        <div class="grid-style" @click="openDoorByDevicenum(1)"><span>1</span></div><a-button>打开柜门</a-button></a-col>-->
-          <div class="grid-style" @click="openDoorByDevicenum(1)">
-            <span>1（打开柜门）</span>
-  <!--          <a-button>打开柜门</a-button>-->
-          </div>
-
-        </a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(9)">9（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(17)">17（打开柜门）</div></a-col>
-      </a-row>
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(2)">2（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="screen-style" style="border-left: 2px solid #000000;border-top: 2px solid #000000;border-right: 2px solid #000000">&nbsp;</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(18)">18（打开柜门）</div></a-col>
-      </a-row>
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(3)">3（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="screen-style" style="border-left: 2px solid #000000;border-right: 2px solid #000000">屏幕</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(19)">19（打开柜门）</div></a-col>
-      </a-row>
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(4)">4（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="screen-style" style="border-left: 2px solid #000000;border-bottom: 2px solid #000000;border-right: 2px solid #000000">&nbsp;</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(20)">20（打开柜门）</div></a-col>
-      </a-row>
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(5)">5（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(13)">13（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(21)">21（打开柜门）</div></a-col>
-      </a-row>
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(6)">6（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(14)">14（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(22)">22（打开柜门）</div></a-col>
-      </a-row>
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(7)">7（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(15)">15（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(23)">23（打开柜门）</div></a-col>
-      </a-row>
-      <a-row :gutter="16" type="flex" justify="space-around" align="middle">
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(8)">8（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(16)">16（打开柜门）</div></a-col>
-        <a-col :span="7"><div class="grid-style" @click="openDoorByDevicenum(24)">24（打开柜门）</div></a-col>
-      </a-row>
-      </a-spin>
-    </j-modal>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @cancel="handleCancel"
+    cancelText="关闭"
+    :footer="null">
+    <div class="lockers">
+      <div class="locker" v-for="(locker,idx) in lockerList" :key="idx">
+      <div class="col" v-for="(item,index) in locker " :key="index">
+				<div v-for="(item1,index1) in item" :key="index1">
+					<div class="row" @click="openDoorByDevicenum(item1)">
+						{{item1}}（打开柜门）
+					</div>
+				</div>
+			</div>
+    </div>
+    </div>
+    
+  </j-modal>
 </template>
 
 <script>
@@ -71,33 +33,29 @@ export default {
       width: 600,
       visible: false,
       devicenum: "",
-      confirmLoading: false,
-      list: [
-        {number: 1, state: 1},
-        {number: 2, state: 1},
-        {number: 3, state: 1},
-        {number: 4, state: 1},
-        {number: 5, state: 1},
-        {number: 6, state: 1},
-        {number: 7, state: 1},
-        {number: 8, state: 1},
-        {number: 9, state: 1},
-        {number: 10, state: 1},
-        {number: 11, state: 1},
-        {number: 12, state: 1},
-        {number: 13, state: 1},
-        {number: 14, state: 1},
-        {number: 15, state: 1},
-        {number: 16, state: 1},
-        {number: 17, state: 1},
-        {number: 18, state: 1},
-        {number: 19, state: 1},
-        {number: 20, state: 1},
-        {number: 21, state: 1},
-      ]
+      lockerList:[]
     }
   },
+  created() {
+
+    // 后期接口拿柜子数量
+    let cabinetNum = 48
+    let arr = Array(cabinetNum).fill(0).map((item,idx) => idx + 1)
+    this.lockerList = this.chunk(arr,24).map(item => this.chunk(item,8))
+    this.width = this.lockerList.length * 560
+  },
   methods: {
+    // 拆分数组 array
+    chunk (array, size) {
+				let result = [];
+				let x = 0
+				for (let x = 0; x < Math.ceil(array.length / size); x++) {
+					let start = x * size;
+					let end = start + size;
+					result.push(array.slice(start, end));
+				}
+				return result;
+			},
     show(record){
       this.devicenum = record.lockerCode;
       this.visible = true;
@@ -107,31 +65,69 @@ export default {
     },
     openDoorByDevicenum(openNum){
       const that = this;
-      that.confirmLoading = true;
-      setTimeout(() => {
 
-        let data = {
-          "devicenum":this.devicenum,
-          "openNum":openNum,
-          "openType":0
+      let data = {
+        "devicenum":this.devicenum,
+        "openNum":openNum,
+        "openType":0
+      }
+
+      httpAction("/api/IoT/openDoorByDevicenum", data, 'post').then((res)=>{
+        let json = JSON.parse(res);
+        if (json.code === 0) {
+          that.$message.success("开门成功")
+        } else {
+          that.$message.warning(json.message);
         }
-
-        httpAction("/api/newIoT/openDoor", data, 'post').then((res)=>{
-          if (res.code === 0) {
-            that.$message.success("开门成功")
-          } else {
-            that.$message.warning(res.message);
-          }
-        })
-
-        that.confirmLoading = false;
-      }, 700)
+      })
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.lockers {
+  display: flex;
+  justify-content: center;
+}
+.locker {
+		box-sizing: border-box;
+		margin:0 15px;
+		background-color: #fff;
+		box-shadow: 2px 4px 8px -3px rgb(144, 138, 138);
+		display: flex;
+.col {
+			display: flex;
+			flex-direction: column;
+			width: 160px;
+
+			.row {
+        cursor: pointer;
+				box-sizing: border-box;
+				height: 70px;
+				color: #fff;
+				display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #2eabff;
+        margin:5px 10px;
+			}
+			
+			.row-screen{
+				box-sizing: border-box;
+				width: 230px;
+				height: 100px;
+				background-color: #F8F8F8;
+				
+				.title{
+					font-size: 42px;
+					font-weight: bold;
+					line-height: 100px;
+					text-align: center;
+				}
+			}
+		}
+  }
  .screen-style{
    width: 100%;
    height: 70px;
