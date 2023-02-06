b200d3b24d87df2fbcd1b5b29a293eac11d2185c
USER-20220902BJ\Administrator <774401914@qq.com>
2023-02-06 09:47:45 +0800

    后台站点

---
+++ src/views/shoesClean/ShoeSitemanager/modules/SetPercentage.vue
@@ -0,0 +1,142 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @cancel="handleCancel"
+    cancelText="关闭"
+    :footer="null"
+  >
+<!--    <j-form-container :disabled="formDisabled">-->
+      <a-form-model ref="form" :model="model" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" :rules="validatorRules">
+  <!--      <a-form-item label="机柜收益">-->
+        <!--        <a-input autocomplete="off"-->
+        <!--          v-decorator="['note', { rules: [{ required: true, message: '请输入1-100之间的整数!' },{ pattern: /^([1-9][0-9]{0,1}|100)$/, message: '请输入1-100之间的整数!'},] }]"-->
+        <!--        />-->
+        <!--      </a-form-item>-->
+        <a-form-model-item label=" 站点收益" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="percentage">
+          <a-input v-model="model.percentage" placeholder="请输入1-100的整数" autocomplete="off" suffix="%"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label=" 配送费设置" :labelCol="labelCol" :wrapperCol="wrapperCol" >
+          (1) 取鞋：同一订单第一双鞋配送费
+          <a-input-number placeholder="输入配送费" style="width: 80px" v-model="model.firstNum" :min="0"
+                          size="small"/>
+          元，每增加一双鞋增加
+          <a-input-number placeholder="输入配送费" style="width: 80px" v-model="model.secondNum" :min="0"
+                          size="small"/>
+          元。
+          <br/>
+          (2) 送鞋：一双鞋配送费
+          <a-input-number placeholder="输入配送费" style="width: 80px" v-model="model.sendNum" :min="0"
+                          size="small"/>
+          元。
+        </a-form-model-item>
+
+        <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+          <a-button type="primary" html-type="submit" @click="setPercentage()">
+            提交
+          </a-button>
+        </a-form-item>
+      </a-form-model>
+<!--    </j-form-container>-->
+  </j-modal>
+</template>
+
+<script>
+import {httpAction} from "@api/manage";
+
+export default {
+  name: "SetPercentage.vue",
+  props: {
+    //表单禁用
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false
+    }
+  },
+  data () {
+    return {
+      model:{firstNum:"",
+        secondNum:"",
+        sendNum:"",
+        percentage:"",
+
+      },
+      title: '机柜收益设置',
+      width: 600,
+      visible: false,
+      lockerId: "",
+      validatorRules: {
+        percentage: [
+          { required: true, message: '请输入1-100之间的整数!'},
+          { pattern: /^([1-9][0-9]{0,1}|100)$/, message: '请输入1-100之间的整数!'},
+        ],
+        num: [
+          { required: true, message: '请输入配送费!'},
+          { pattern: /^[0-9]*$/, message: '请输入配送费!'},
+        ],
+      },
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    }
+  },
+  computed: {
+    formDisabled(){
+      return this.disabled
+    },
+  },
+  methods: {
+    show(record) {
+      Object.assign(this.model, record);
+      this.lockerId = record.lockerId;
+      this.model.firstNum = record.courierCost/100;
+      this.model.secondNum = record.courierCostIncrease/100;
+      this.model.sendNum = record.courierSendCost/100;
+      this.visible = true;
+      this.model.percentage= (record.percentage * 100).toFixed(0);
+    },
+    handleCancel() {
+      this.visible = false;
+    },
+    setPercentage() {
+      const that = this;
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          let data = {
+            "lockerId": that.lockerId,
+            "percentage": that.model.percentage,
+            "courierCost": that.model.firstNum*100,
+            "courierCostIncrease": that.model.secondNum*100,
+            "courierSendCost": that.model.sendNum*100,
+          }
+
+          httpAction("/shoes/shoeLocker/setPercentageBySitemanager", data, 'post').then((res) => {
+            if (res.code === 200) {
+              that.$message.success("设置成功")
+              this.visible = false;
+              that.$emit('ok');
+            } else {
+              that.$message.warning(res.message);
+            }
+          })
+        }
+      })
+
+
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/shoesClean/ShoeSitemanager/modules/ShoeSitemanagerAudit.vue
@@ -0,0 +1,107 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @cancel="handleCancel"
+    cancelText="关闭"
+    @ok="handleOk"
+  >
+
+
+    <h2>申请人：{{ name }}</h2>
+    <h2>提现金额：{{ amount }}</h2>
+
+    <!--      <a-form-model-item label="审核" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="status">-->
+    <!--        <j-dict-select-tag type="radio" v-model="model.status"  dictCode="shoe_courier_status" placeholder="请选择登录状态" />-->
+    <!--      </a-form-model-item>-->
+    <h2>审核：
+      <j-dict-select-tag type="radio" v-model="model.status" dictCode="withdrawal_audit"/>
+    </h2>
+
+      <h2 v-if="this.model.status==1">拒绝原因(必填)：
+        <a-textarea v-model="model.note"  rows="4" />
+      </h2>
+
+
+
+
+  </j-modal>
+
+</template>
+
+<script>
+import {httpAction, putAction} from "@api/manage";
+
+export default {
+  name: "ShoeInvestorsAudit",
+  props: {
+    //表单禁用
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false
+    }
+  },
+  data() {
+    return {
+      title: '提现详情',
+      width: 600,
+      amount: '',
+      name: '',
+      id: '',
+      visible: false,
+      model: {
+        status: 0
+      },
+
+    }
+  },
+  methods: {
+    handleOk() {
+      const that = this;
+      let data = {
+        "sitemanagerWithdrawalId": that.id,
+        "status": that.model.status,
+        "note": that.model.note,
+        "amount": that.amount * 100,
+      };
+     if (that.model.note==null&&that.model.status==1){
+       that.$message.error('请填写拒绝原因')
+     }else {
+       putAction("/shoes/shoeLocker/updateWithdrawalAuditStatus", data).then((res) => {
+         if (res.code === 200) {
+           this.visible = false;
+           that.$message.success('提交成功')
+           that.$emit('ok');
+           that.model.note=null;
+         }
+       })
+     }
+
+
+    },
+    handleCancel() {
+      this.visible = false;
+    },
+    show(record) {
+      this.visible = true;
+
+      httpAction("/shoes/shoeLocker/withdrawalDetail?id=" + record.sitemanagerWithdrawalId, null, "get").then((res) => {
+        if (res) {
+          this.visible = true;
+          this.id = res.result.sitemanagerWithdrawalId;
+          this.name = res.result.name;
+          this.amount = res.result.amount;
+        }
+      })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
