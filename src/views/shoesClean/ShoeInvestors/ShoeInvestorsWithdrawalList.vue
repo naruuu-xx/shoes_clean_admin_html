79ee1d0e3acc4f911e82b5e658b543ffe4b8618d
SCSP-WZY <782105758@qq.com>
2023-03-01 09:43:29 +0800

    推广的统计页面和提现页面；

--- src/views/shoesClean/ShoeInvestors/ShoeInvestorsWithdrawalList.vue
+++ src/views/shoesClean/ShoeInvestors/ShoeInvestorsWithdrawalList.vue
@@ -24,17 +24,12 @@
             <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
               <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
               <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
-              <!--              <a @click="handleToggleSearch" style="margin-left: 8px">-->
-              <!--                {{ toggleSearchStatus ? '收起' : '展开' }}-->
-              <!--                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>-->
-              <!--              </a>-->
             </span>
           </a-col>
         </a-row>
       </a-form>
     </div>
     <!-- 查询区域-END -->
-
 
 
     <!-- table区域-begin -->
@@ -46,7 +41,7 @@
         size="middle"
         :scroll="{x:true}"
         bordered
-        rowKey="goodsId"
+        rowKey="investorsWithdrawalId"
         :columns="columns"
         :dataSource="dataSource"
         :pagination="ipagination"
@@ -63,43 +58,44 @@
           <a @click="handleAudit(record)" v-if="record.status==0" v-has="'Withdrawal:Audit'">审核</a>
           <a-divider type="vertical" v-if="record.status==1" v-has="'Withdrawal:Transfer'"/>
           <a @click="handleTransfer(record)" v-if="record.status==1" v-has="'Withdrawal:Transfer'">确认</a>
-
-
         </span>
 
       </a-table>
     </div>
-  <ShoeInvestorsWithdrawalDetail ref="ShoeInvestorsWithdrawalDetail" ></ShoeInvestorsWithdrawalDetail>
-  <ShoeInvestorsAudit ref="ShoeInvestorsAudit" @ok="modalFormOk"></ShoeInvestorsAudit>
+    <ShoeInvestorsWithdrawalDetail ref="ShoeInvestorsWithdrawalDetail"></ShoeInvestorsWithdrawalDetail>
+    <ShoeInvestorsAudit ref="ShoeInvestorsAudit" @ok="modalFormOk"></ShoeInvestorsAudit>
     <ShoeInvestorsTransfer ref="ShoeInvestorsTransfer" @ok="modalFormOk"></ShoeInvestorsTransfer>
   </a-card>
 </template>
 
 <script>
-import { mixinDevice } from '@/utils/mixin'
-import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import {mixinDevice} from '@/utils/mixin'
+import {JeecgListMixin} from '@/mixins/JeecgListMixin'
 import '@/assets/less/TableExpand.less'
 import {filterDictTextByCache, filterMultiDictText} from '@comp/dict/JDictSelectUtil'
-import { getFileAccessHttpUrl, httpAction } from '@api/manage'
+import {getFileAccessHttpUrl, httpAction} from '@api/manage'
 import ShoeInvestorsWithdrawalDetail from "@views/shoesClean/ShoeInvestors/modules/ShoeInvestorsWithdrawalDetail";
 import ShoeInvestorsAudit from "@views/shoesClean/ShoeInvestors/modules/ShoeInvestorsAudit";
 import ShoeInvestorsTransfer from "@views/shoesClean/ShoeInvestors/modules/ShoeInvestorsTransfer";
 
 export default {
-name: "ShoeInvestorsWithdrawalList",
-  mixins:[JeecgListMixin, mixinDevice],
+  name: "ShoeInvestorsWithdrawalList",
+  mixins: [JeecgListMixin, mixinDevice],
   components: {
     ShoeInvestorsWithdrawalDetail,
     ShoeInvestorsAudit,
     ShoeInvestorsTransfer
   },
-  data(){
+  data() {
     return {
-      dataSource:[],
-      statusOptionList: [{"value":"0", "name":"审核中"}, {"value":"1", "name":"提现中"}, {"value":"2", "name":"审核失败"}, {"value":"3", "name":"已提现"}],
+      dataSource: [],
+      statusOptionList: [{"value": "0", "name": "审核中"}, {"value": "1", "name": "提现中"}, {
+        "value": "2",
+        "name": "审核失败"
+      }, {"value": "3", "name": "已提现"}],
       description: 'shoe_investors_withdrawal管理页面',
-      queryParam:{
-        status:''
+      queryParam: {
+        status: ''
       },
       // 表头
       columns: [
@@ -123,10 +119,10 @@ name: "ShoeInvestorsWithdrawalList",
           align: "center",
           dataIndex: 'auditTime',
           customRender: (text) => {
-            if(text == null){
+            if (text == null) {
               let show = "-";
               return show;
-            }else return text;
+            } else return text;
           },
         },
         {
@@ -134,10 +130,10 @@ name: "ShoeInvestorsWithdrawalList",
           align: "center",
           dataIndex: 'transferTime',
           customRender: (text) => {
-           if(text == null){
-             let show = "-";
-             return show;
-           }else return text;
+            if (text == null) {
+              let show = "-";
+              return show;
+            } else return text;
           },
 
         },
@@ -146,10 +142,10 @@ name: "ShoeInvestorsWithdrawalList",
           align: "center",
           dataIndex: 'auditName',
           customRender: (text) => {
-            if(text == null){
+            if (text == null) {
               let show = "-";
               return show;
-            }else return text;
+            } else return text;
 
           },
         },
@@ -175,22 +171,22 @@ name: "ShoeInvestorsWithdrawalList",
       },
     }
   },
-  create(){
+  create() {
 
   },
   methods: {
-    showDetail(record){
+    showDetail(record) {
       this.$refs.ShoeInvestorsWithdrawalDetail.show(record);
     },
-    handleAudit(record){
+    handleAudit(record) {
       this.$refs.ShoeInvestorsAudit.show(record);
     },
-    handleTransfer(record){
+    handleTransfer(record) {
       this.$refs.ShoeInvestorsTransfer.show(record);
     }
 
 
-}
+  }
 }
 </script>
 
