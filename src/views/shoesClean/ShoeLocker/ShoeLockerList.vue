8b1daed24d4e728a101b4e50c35f5ed92af1556a
DESKTOP-4EDG6UN\UU <wang_guanxing2022@163.com>
2023-03-22 18:14:26 +0800

    机柜添加环境图

--- src/views/shoesClean/ShoeLocker/ShoeLockerList.vue
+++ src/views/shoesClean/ShoeLocker/ShoeLockerList.vue
@@ -149,6 +149,9 @@
                     <a>一键开柜</a>
                    </a-popconfirm>
               </a-menu-item>
+              <a-menu-item>
+                <a @click="sceneGraph(record)">场景图</a>
+             </a-menu-item>
             </a-menu>
           </a-dropdown>
 
@@ -173,6 +176,7 @@
     <shoe-locker-modal ref="modalForm" @ok="modalFormOk"></shoe-locker-modal>
     <shoe-locker-grid-modal ref="gridModal"></shoe-locker-grid-modal>
     <set-percentage ref="setPercentage" @ok="modalFormOk"></set-percentage>
+    <shoe-locker-img-modal ref="imgModal" @ok="modalFormOk"></shoe-locker-img-modal>
   </a-card>
 </template>
 
@@ -183,6 +187,7 @@ import {mixinDevice} from '@/utils/mixin'
 import {JeecgListMixin} from '@/mixins/JeecgListMixin'
 import ShoeLockerModal from './modules/ShoeLockerModal'
 import shoeLockerGridModal from './modules/shoeLockerGridModal'
+import ShoeLockerImgModal from './modules/ShoeLockerImgModal'
 import Area from '@/components/_util/Area'
 import {filterDictTextByCache} from "@comp/dict/JDictSelectUtil";
 import {httpAction} from "../../../api/manage";
@@ -196,7 +201,8 @@ export default {
   components: {
     ShoeLockerModal,
     shoeLockerGridModal,
-    SetPercentage
+    SetPercentage,
+    ShoeLockerImgModal
   },
   data() {
     return {
@@ -397,6 +403,9 @@ export default {
     setPercentage(record){
       this.$refs.setPercentage.show(record);
 
+    },
+    sceneGraph(record) {
+      this.$refs.imgModal.show(record);
     },
     openAllDoor(lockerCode) {
       const that = this;
