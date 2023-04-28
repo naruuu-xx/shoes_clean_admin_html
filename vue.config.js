bbc5e181baaca6b1a237597f10e5c3f80fae5c4a
yzm <1371924520@qq.com>
2023-04-28 17:47:14 +0800

    dev前端配置https

--- vue.config.js
+++ vue.config.js
@@ -87,8 +87,8 @@ module.exports = {
   },
 
   devServer: {
-    // https: true,
-    // host: '0.0.0.0',
+    https: true,
+    host: '0.0.0.0',
     port: 3000,
     // hot: true,
     // disableHostCheck: true,
