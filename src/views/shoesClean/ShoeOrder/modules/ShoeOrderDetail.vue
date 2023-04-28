024ea8221c6362360525b5b7c4e39fc6e8830612
SCSP-WZY <782105758@qq.com>
2023-04-28 09:26:41 +0800

    修改“查看鞋子”位置

--- src/views/shoesClean/ShoeOrder/modules/ShoeOrderDetail.vue
+++ src/views/shoesClean/ShoeOrder/modules/ShoeOrderDetail.vue
@@ -122,7 +122,7 @@
             <div class="table-main-cell-status">{{ item.title }}</div>
             <div class="table-main-cell-des">
               {{ item.msg }}
-              <a v-if="item.status == 1" @click="showShoeImages(orderImagesList)" style="margin-left: 10px; text-decoration: underline">查看鞋子照片</a>
+              <a v-if="item.status == 0" @click="showShoeImages(orderImagesList)" style="margin-left: 10px; text-decoration: underline">查看鞋子照片</a>
               <a v-if="item.status == 6" @click="showShoeImages(factoryInImages)" style="margin-left: 10px; text-decoration: underline">查看入库照片</a>
             </div>
           </div>
