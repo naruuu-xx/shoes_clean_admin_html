a8d6d7d8d4fa6fb2d178c3a72fb7fb8b2e8a7ffd
yzm <1371924520@qq.com>
2023-05-06 14:10:21 +0800

    签到设置 配置规则

--- src/views/shoesClean/shoesSignIn/shoesSignInSet.vue
+++ src/views/shoesClean/shoesSignIn/shoesSignInSet.vue
@@ -1,10 +1,11 @@
 <template>
   <a-card :bordered="false">
     <a-row>
-      <a-col :xs="24" :sm="24" :md="6">签到周期:<a-input-number v-model="period" placeholder="请输入签到周期" @change="v => period = isNaN(parseInt(v)) ? 1 : parseInt(v)" :min="1"/>天</a-col>
+      <a-col :xs="24" :sm="24" :md="6">签到周期:<a-input-number @blur="periodBlur" v-model="period" placeholder="请输入签到周期"
+          @change="v => period = isNaN(parseInt(v)) ? 1 : parseInt(v)" :min="1" />天</a-col>
       <a-col :xs="24" :sm="24" :md="18">
         <a-space>
-          <a-button>确定</a-button>
+          <a-button type="primary">确定</a-button>
           <a-button>重置</a-button>
         </a-space>
       </a-col>
@@ -12,21 +13,18 @@
 
     <a-row>
       <div class="table">
-        <table border="1px" cellpadding="10px">
+        <table border="1px" cellpadding="10px" v-for="(integral, integralIdx) in integralList2" :key="integralIdx">
           <tr>
             <th>天数</th>
-            <th v-for="(val,idx) in integral" :key="idx">第{{idx+1}}天</th>
+            <th v-for="(val, idx) in integral" :key="idx">第{{ toChineseNum(integralIdx * 7 + idx + 1)}}天</th>
           </tr>
           <tr>
             <td>积分值</td>
-            <td v-for="(val,idx) in integral" :key="idx">
-              <a-input-number v-model="integral[idx]" placeholder="请输入积分值" @change="v => integral[idx] = isNaN(parseInt(v)) ? 1 : parseInt(v)" :min="0"/>
+            <td v-for="(val, idx) in integral" :key="idx">
+              <a-input-number v-model="integral[idx].integral" placeholder="请输入积分值"
+                @change="v => integral[idx].integral = isNaN(parseInt(v)) ? 1 : parseInt(v)" :min="0" />
             </td>
           </tr>
-        </table>
-        <table border="1px" cellpadding="10px">
-          <tr><th>天数</th><th>姓名</th><th>年龄</th></tr>
-          <tr><td>积分值</td><td>憨憨</td><td>21</td></tr>
         </table>
       </div>
     </a-row>
@@ -34,47 +32,267 @@
     <a-row>
       <a-space>
         <span>特殊天数设置:</span>
-        <a-button>新增</a-button>
+        <a-button @click="onAddSpecialDay" type="primary">新增</a-button>
       </a-space>
     </a-row>
+
+    <a-row v-for="(specialDay,idx) in specialDays" :key="idx">
+      <div class="specialDays">
+        <div class="specialDays-top" :data-idx="`(${idx+1})`">
+          第<a-input-number @blur="specialDayBlur(idx)" v-model="specialDays[idx].day" placeholder="请输入天数" @change="v => specialDays[idx].day = isNaN(parseInt(v)) ? 1 : parseInt(v)" :min="1" />天
+          <a-space style="margin-left: 24px;">
+            <a-button @click="onAddCoupon(idx)" type="primary" :disabled="specialDays[idx].day == '' || (!!specialDays[idx].coupons.length && specialDays[idx].coupons.some(coupon => coupon.id == ''))">新增</a-button>
+            <a-button @click="onDelSpecialDay(idx)" type="danger">删除</a-button>
+          </a-space>
+        </div>
+        <div class="specialDays-sub">
+          <span style="flex-shrink: 0;margin-right: 16px;">描述:</span> <a-input v-model="specialDays[idx].describe"></a-input>
+        </div>
+        <div class="specialDays-table">
+          <a-table bordered :columns="columns" :data-source="specialDays[idx].coupons" :pagination="false" rowKey="uuid">
+            <div slot="type" slot-scope="text, record, tableIdx">
+              <a-select v-model="specialDays[idx].coupons[tableIdx].type"  style="width: 240px" :options="typeOptions" @change="changeType($event,idx)">
+              </a-select>
+            </div>
+            <div slot="id" slot-scope="text, record, tableIdx">
+              <XfSelect
+                :list="weekList"
+                @change="checkedSelect"
+                @changeList="changeSelect"
+                v-model="specialDays[idx].coupons[tableIdx].id"
+                :url='`/shoes/shoeUser/getCouponOrCardBagOrTimecard?type=0`'
+                style="width: 80%;"
+              >
+              </XfSelect>
+            </div>
+            <div slot="num" slot-scope="text, record, tableIdx">
+              <a-input-number v-model="specialDays[idx].coupons[tableIdx].num" placeholder="请输入数量"
+                @change="v => specialDays[idx].coupons[tableIdx].num = isNaN(parseInt(v)) ? 1 : parseInt(v)" :min="0" />
+            </div>
+            <span slot="action" slot-scope="text, record,tableIdx">
+              <a-button type="link" style="color: #ff4d4f" @click="onDelCoupon(idx,tableIdx)">
+                删除
+              </a-button>
+            </span>
+          </a-table>
+        </div>
+      </div>
+    </a-row>
+
+    <div class="btns">
+      <a-button @click="onCancel">取消</a-button>
+      <a-button type="primary" @click="onSave">保存</a-button>
+    </div>
   </a-card>
 </template>
 
 <script>
 
+import {toChineseNum} from '@/utils/util'
+import XfSelect from '@/components/Xf/XfSelect'
+export default {
+  name: 'ShoeCouponList',
+  mixins: [],
+  components: {
+    XfSelect
+  },
+  data() {
+    return {
+      period: '',
+      integralList2: [],
+      integralList:[],
+      columns:[
+        {
+          title:'类型',
+          align:"center",
+          dataIndex: 'type',
+          scopedSlots: { customRender: 'type' },
+          width: '140px'
+        },
+        {
+          title:'名称',
+          align:"center",
+          dataIndex: 'id',
+          scopedSlots: { customRender: 'id' },
+        },
+        {
+          title:'数量',
+          align:"center",
+          dataIndex: 'num',
+          scopedSlots: { customRender: 'num' },
+          width: '120px'
+        },
+        {
+          title: '操作',
+          align:"center",
+          scopedSlots: { customRender: 'action' },
+          width: '120px'
+        },
+      ],
+      specialDays:[],
+      typeOptions:[
+        {
+          label:'优惠券',
+          value:'1'
+        },
+        {
+          label:'卡包',
+          value:'0'
+        }
+      ],
+      weekList:[],
+    }
+  },
+  created() {
 
-  export default {
-    name: 'ShoeCouponList',
-    mixins:[],
-    components: {
+  },
+  computed: {
 
-    },
-    data () {
-      return {
-        period:'',
-        integral:[
-          0,0,0,0,0,0,0
-        ]
+  },
+  watch: {
+    period: {
+      handler(val) {
+
       }
     },
-    created() {
+  },
+  methods: {
+    getUuid() {
+      return Math.random().toString(16).substring(2)
+    },
+    checkedSelect(val) {
+    },
+    changeSelect(data) {
+      this.weekList = data.records.map(item => ({
+        label: item.name,
+        value: +item.id
+      }));
+    },
+    changeType(val,idx) {
+      console.log('changeType',val,idx);
+    },
+    // 添加优惠券或者卡包
+    onAddCoupon(idx) {
+      this.specialDays[idx].coupons.push(
+        {
+          type:'0', // 卡包
+          name:'卡包名称',
+          id:'',
+          uuid:this.getUuid(),
+          num:1, // 数量
+        }
+      )
+    },
+    // 删除优惠券或者卡包
+    onDelCoupon(idx,tableIdx) {
+      this.specialDays[idx].coupons.splice(tableIdx, 1)
+    },
+    // 
+    specialDayBlur(idx) {
+      console.log(333,idx);
+    },
+    // 删除特殊天数
+    onDelSpecialDay(idx) {
+      this.specialDays.splice(idx, 1)
+    },
+    // 添加`特殊天数
+    onAddSpecialDay() {
+      this.specialDays.push(
+        {
+          day:'', // 天数
+          describe:'',
+          coupons:[]
+        }
+      )
+    },
+    // 连续签到天数移除焦点
+    periodBlur() {
+      let oldArr = this.integralList2.flat()
+      let oldArrLength = oldArr.length
+      this.integralList = new Array(parseInt(this.period)).fill(0).map((item, idx) => {
+        if (oldArrLength > idx) {
+          return oldArr[idx]
+        } else {
+          return {
+            integral: 0, // 积分
+            describe: '',
+            coupons: []
+          }
+        }
+
+      })
+      this.integralList2 = this.chunk(this.integralList, 7)
+    },
+    chunk(array, size) {
+      let result = []
+      let x = 0
+      for (let x = 0; x < Math.ceil(array.length / size); x++) {
+        let start = x * size
+        let end = start + size
+        result.push(array.slice(start, end))
+      }
+      return result
+    },
+    onCancel() {
 
     },
-    computed: {
-      
+    onSave() {
+      console.log('specialDays',this.specialDays);
+      console.log('integralList',this.integralList);
+      console.log('integralList2',this.integralList2);
     },
-    methods: {
-      
+    toChineseNum
+  }
+}
+</script>
+<style scoped lang="less">
+@import '~@assets/less/common.less';
+.btns {
+  display: flex;
+  justify-content: center;
+}
+
+table,
+th,
+td {
+  border-color: #f5f5f5;
+}
+
+th {
+  min-width: 64px;
+}
+
+.table {
+  text-align: center;
+  margin: 20px 0;
+  overflow-x: scroll;
+  max-width: 844px;
+}
+
+.specialDays {
+  margin-left: 34px;
+  width: 770px;
+  &-top {
+    margin: 20px 0;
+    position: relative;
+    &::after {
+      position: absolute;
+      content: attr(data-idx);
+      top: 50%;
+      transform: translateY(-50%);
+      left: -24px;
     }
   }
-</script>
-<style scoped>
-  @import '~@assets/less/common.less';
-  table,th,td {
-    border-color: #f5f5f5;
+  &-sub {
+    display: flex;
+    margin-bottom: 20px;
+    align-items: center;
   }
-  .table {
-    text-align: center;
+  &-table {
     
   }
+
+}
+
+
 </style>
