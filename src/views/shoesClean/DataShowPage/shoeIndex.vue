ff7f6d63cc2ca9f60c52a0a17aacdf4c7ad73970
yzm <1371924520@qq.com>
2023-05-17 13:49:24 +0800

    修改数据宽度变窄一些

--- src/views/shoesClean/DataShowPage/shoeIndex.vue
+++ src/views/shoesClean/DataShowPage/shoeIndex.vue
@@ -414,7 +414,7 @@ export default {
     &-title {
       background: #F5F5F5;
       padding: 16px 0;
-      padding-right: 90px;
+      padding-right: 40px;
       font-size: 16px;
       font-weight: 500;
       line-height: 22px;
