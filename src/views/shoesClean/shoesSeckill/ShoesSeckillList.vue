6fa6730f4408d69f0d46596a21f932c06c3cbdbf
yzm <1371924520@qq.com>
2023-05-22 16:25:41 +0800

    限时秒杀

---
+++ src/views/shoesClean/shoesSeckill/ShoesSeckillList.vue
@@ -0,0 +1,273 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline" @keyup.enter.native="searchQuery">
+        <a-row :gutter="24">
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <a-form-item label="名称">
+              <a-input placeholder="请输入商品名称" v-model="queryParam.title"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <a-form-item label="状态">
+              <a-select v-model="queryParam.statusText"  :allowClear = "true">
+                <a-select-option v-for="i in statusList" :value="i.statusText" :key="i.status">{{i.statusText}}</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <!-- 查询区域-END -->
+
+    <!-- 操作按钮区域 -->
+    <div class="table-operator">
+      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+    </div>
+
+    <!-- table区域-begin -->
+    <div>
+
+      <a-table
+        ref="table"
+        size="middle"
+        :scroll="{x:true}"
+        bordered
+        rowKey="goodsId"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        class="j-table-force-nowrap"
+        @change="handleTableChange">
+
+        <template slot="htmlSlot" slot-scope="text">
+          <div v-html="text"></div>
+        </template>
+
+        <span slot="action" slot-scope="text, record">
+          <a @click="handleEdit(record)">编辑</a>
+
+          <a-divider type="vertical" />
+          <a-dropdown>
+            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a @click="handleDetail(record)">详情</a>
+              </a-menu-item>
+              <template v-if="!matchState(record.goodsId,/[12]/)">
+                <a-menu-item>
+                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.goodsId)">
+                  <a>删除</a>
+                </a-popconfirm>
+              </a-menu-item>
+              </template>
+
+            </a-menu>
+          </a-dropdown>
+        </span>
+
+      </a-table>
+    </div>
+
+    <ShoesSeckillModal ref="modalForm" @ok="modalFormOk"></ShoesSeckillModal>
+  </a-card>
+</template>
+
+<script>
+
+  import '@/assets/less/TableExpand.less'
+  import { mixinDevice } from '@/utils/mixin'
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+  import ShoesSeckillModal from './modules/ShoesSeckillModal'
+  import { filterMultiDictText } from '@comp/dict/JDictSelectUtil'
+  import { getFileAccessHttpUrl, httpAction } from '@api/manage'
+
+  export default {
+    name: 'ShoeGoodsList',
+    mixins:[JeecgListMixin, mixinDevice],
+    components: {
+      ShoesSeckillModal
+    },
+    data () {
+      function typerender(text) {
+        if(text === "ordinary"){
+          return "普洗";
+        }
+        else if(text === "fine"){
+          return "精洗";
+        }
+        else{
+          return "修复";
+        }
+      }
+
+      function imagerender(text) {
+        if(text && text.indexOf(",")>0){
+          text = text.substring(0,text.indexOf(","))
+        }
+        return getFileAccessHttpUrl(text)
+      }
+
+      return {
+        description: 'shoe_goods管理页面',
+        // 表头
+        columns: [
+          {
+            title:'ID',
+            align:"center",
+            dataIndex: 'goodsId'
+          },
+          {
+            title:'商品名称',
+            align:"center",
+            dataIndex: 'title'
+          },
+          {
+            title:'商品分类',
+            align:"center",
+            dataIndex: 'name'
+          },
+          {
+            title:'类型',
+            align:"center",
+            dataIndex: 'type',
+            customRender:(text) => (typerender(text)),
+          },
+          // {
+          //   title:'描述',
+          //   align:"center",
+          //   dataIndex: 'describe'
+          // },
+          // {
+          //   title:'详情',
+          //   align:"center",
+          //   dataIndex: 'content'
+          // },
+          {
+            title:'销量',
+            align:"center",
+            dataIndex: 'sale'
+          },
+          {
+            title:'浏览量',
+            align:"center",
+            dataIndex: 'pv'
+          },
+          {
+            title:'权重',
+            align:"center",
+            dataIndex: 'weight'
+          },
+          {
+            title:'状态',
+            align:"center",
+            dataIndex: 'status',
+            customRender: (text) => (filterMultiDictText(this.dictOptions['status'], text)),
+          },
+          // {
+          //   title:'删除状态:0=正常,1=删除',
+          //   align:"center",
+          //   dataIndex: 'delFlag'
+          // },
+          // {
+          //   title:'删除时间',
+          //   align:"center",
+          //   dataIndex: 'deleteTime',
+          //   customRender:function (text) {
+          //     return !text?"":(text.length>10?text.substr(0,10):text)
+          //   }
+          // },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            align:"center",
+            fixed:"right",
+            width:147,
+            scopedSlots: { customRender: 'action' }
+          }
+        ],
+        url: {
+          list: "/shoes/shoeGoods/list",
+          delete: "/shoes/shoeGoods/delete",
+          deleteBatch: "/shoes/shoeGoods/deleteBatch",
+          exportXlsUrl: "/shoes/shoeGoods/exportXls",
+          importExcelUrl: "shoes/shoeGoods/importExcel",
+
+        },
+        dictOptions:{},
+        superFieldList:[],
+        typeList:[],
+        statusList:[]
+      }
+    },
+    created() {
+    this.getSuperFieldList();
+    this.goodsTypeList();
+    this.goodsStatusList();
+    this.$set(this.dictOptions, 'status', [{text:'上架',value:'1'},{text:'下架',value:'0'}]);
+    console.log("----------------");
+    console.log(this.statusList);
+    },
+    computed: {
+      importExcelUrl: function(){
+        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+      },
+    },
+    methods: {
+      initDictConfig(){
+      },
+
+      /**
+       *v-if多判断条件
+       */
+      matchState(state = '', reg) {
+        return !!String(state).match(reg)//返回true/false
+      },
+      getSuperFieldList(){
+        let fieldList=[];
+        fieldList.push({type:'int',value:'goodsId',text:'ID'})
+        fieldList.push({type:'string',value:'type',text:'类型:ordinary=普洗,fine=精洗,repair=修复'})
+        fieldList.push({type:'string',value:'title',text:'商品名称'})
+        fieldList.push({type:'string',value:'image',text:'主图'})
+        fieldList.push({type:'string',value:'describe',text:'描述'})
+        fieldList.push({type:'string',value:'content',text:'详情'})
+        fieldList.push({type:'int',value:'sale',text:'销量'})
+        fieldList.push({type:'int',value:'pv',text:'浏览量'})
+        fieldList.push({type:'int',value:'weight',text:'权重'})
+        fieldList.push({type:'int',value:'status',text:'状态:0=下架,1=上架'})
+        fieldList.push({type:'int',value:'delFlag',text:'删除状态:0=正常,1=删除'})
+        fieldList.push({type:'date',value:'deleteTime',text:'删除时间'})
+        this.superFieldList = fieldList
+      },
+      goodsTypeList(){
+        httpAction("/shoes/shoeGoodsSku/typeList", null, "get").then((res) => {
+          if (res){
+
+            this.typeList = res;
+          }
+        })
+      },
+      goodsStatusList(){
+        httpAction("/shoes/shoeGoodsSku/statusList", null, "get").then((res) => {
+          if (res){
+
+            this.statusList = res;
+          }
+        })
+      }
+    }
+
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less';
+</style>
---
+++ src/views/shoesClean/shoesSeckill/modules/ShoesSeckillForm.vue
@@ -0,0 +1,488 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <j-form-container :disabled="formDisabled">
+      <a-form-model ref="form" slot="detail" :model="model" :rules="validatorRules">
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="商品名称" prop="title">
+              <a-input v-model="model.title" placeholder="请输入商品名称"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="商品描述" prop="describe">
+              <a-input v-model="model.describe" placeholder="请输入描述"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24" >
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="商品规格">
+              <a-button class="editable-add-btn" @click="handleAdd">
+                添加规格
+              </a-button>
+              <a-table bordered :data-source="model.skuTable" :columns="columns"
+                        prop="skuTable" :rowKey="model.uuid">
+                <template slot="skuTitle" slot-scope="text,index, record" prop="skuTitle">
+                  <editable-cell :text="text" @change="onCellChange(record, 'skuTitle', $event)" />
+                </template>
+                <template slot="price" slot-scope="text,index, record" prop="price">
+                  <editable-cell type="number" :text="text" @change="onCellChange(record,'price',$event)" />
+                </template>
+                <template slot="originalPrice" slot-scope="text,index, record" prop="originalPrice">
+                  <editable-cell type="number" :text="text" @change="onCellChange(record,'originalPrice',$event)" />
+                </template>
+                <template slot="skuImage" slot-scope="skuImage,index, record" prop="skuImage">
+                  <j-image-upload v-model="skuImage" @change="onCellChange(record, 'skuImage',$event)" :isMultiple="false" text="上传"></j-image-upload>
+                </template>
+
+
+                <template slot="operation" slot-scope="text, record,index">
+                  <a-popconfirm
+                    title="确定要删除该规格么？"
+                    @confirm="() => onDelete(index,record)"
+                  >
+                    <a href="javascript:;">删除</a>
+                  </a-popconfirm>
+                </template>
+              </a-table>
+            </a-form-model-item>
+          </a-col>
+
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="产品库存" prop="inventory">
+              <a-input-number v-model="model.inventory" :min="1" @change="v => model.inventory = isNaN(parseInt(v)) ? 1 : parseInt(v)" />
+            </a-form-model-item>
+          </a-col>
+
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="每人限购数量" prop="purchaseMotivation">
+              <a-input-number v-model="model.purchaseMotivation" :min="1" @change="v => model.purchaseMotivation = isNaN(parseInt(v)) ? 1 : parseInt(v)" />
+            </a-form-model-item>
+          </a-col>
+
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="轮播图" prop="banner">
+              <j-image-upload v-model="model.banner" :isMultiple="true" text="上传"></j-image-upload>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="详情" prop="content">
+              <JEditor v-model="model.content" placeholder="请输入详情"></JEditor>
+            </a-form-model-item>
+
+          </a-col>
+
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="权重" prop="weight">
+              <a-input-number v-model="model.weight" placeholder="请输入权重" style="width: 100%"/>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="状态" prop="status">
+              <!--              <a-input-number v-model="model.status" placeholder="请输入状态:0=下架,1=上架" style="width: 100%" />-->
+              <j-switch v-model="model.status"></j-switch>
+            </a-form-model-item>
+          </a-col>
+
+          <a-col :span="24">
+            <a-form-model-item label="附加服务项" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="additionalId" >
+              <a-select
+                mode="multiple"
+                show-search
+                placeholder="选择或搜索需要绑定的用户"
+                option-filter-prop="children"
+                style="width: 200px"
+                :filter-option="filterOption"
+                v-model="model.additionalIds"
+              >
+                <a-select-option  v-for="i in additionalList" :value="i.additionalId" :key="i.additionalId">
+                  {{i.name}}
+                </a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+
+        </a-row>
+
+      </a-form-model>
+    </j-form-container>
+  </a-spin>
+</template>
+
+<script>
+import { getAction, httpAction } from '@/api/manage'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import { mixinDevice } from '@/utils/mixin'
+import signMd5Utils from '@/utils/encryption/signMd5Utils'
+import { axios } from '@/utils/request'
+
+
+const EditableCell = {
+  template: `
+    <div class="editable-cell">
+    <div v-if="editable" class="editable-cell-input-wrapper">
+      <a-input :value="value" @change="handleChange" @pressEnter="check" @input="inputChange" v-if="type == 'text'" @blur="editable = false"/>
+      <a-input-number v-model="value" :min="0" @pressEnter="check" @change="handleChange" v-else @blur="editable = false" />
+    </div>
+    <div v-else class="editable-cell-text-wrapper" @click="edit">
+      {{ value || ' ' }}
+      <a-icon type="edit" class="editable-cell-icon"/>
+    </div>
+    </div>
+  `,
+  props: {
+    text: String | Number,
+    type:{
+      type: String,
+      default: 'text'
+    }
+  },
+  model: {
+    prop: "value",
+    event: "input",
+  },
+  data() {
+    return {
+      value: this.text,
+      editable: false,
+    };
+  },
+  watch:{
+    text(value) {
+      this.value = value;
+    }
+  },
+  methods: {
+
+    handleChange(e) {
+      const value = this.type == 'text' ? e.target.value : e
+      console.log(6666,value);
+      this.value = value;
+      
+      this.$emit('change', this.value);
+    },
+    inputChange(e){
+
+
+        this.$emit("input", e.target.value);
+    },
+    check() {
+      this.editable = false;
+      this.$emit('change', this.value);
+    },
+    edit() {
+      this.editable = true;
+    },
+  },
+};
+
+
+
+
+// for(let i in model.skuTable){
+//   data.push(model.skuTable[i]);
+// }
+
+
+
+export default {
+  name: 'ShoeGoodsForm',
+  mixins: [JeecgListMixin, mixinDevice],
+  components: {
+    EditableCell,
+  },
+  props: {
+
+    //表单禁用
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false
+    }
+  },
+  data() {
+
+    return {
+      key: 0,
+
+      editingKey: '',
+
+      model: {
+        skuTable:[],
+      },
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+      confirmLoading: false,
+      validatorRules: {
+        title: [
+          { required: true, message: '请输入商品名称!' },
+        ],
+        content: [
+          { required: true, message: '请输入详情!' },
+        ],
+        weight: [
+          { required: true, message: '请输入权重!' },
+        ],
+        goodsSku: [
+          { required: true, message: '请填写商品规格！' },
+        ],
+        skuTitle: [
+          { required: true, message: '请填写规格名称!' },
+        ],
+        price: [
+          { required: true, message: '测' },
+        ],
+        status: [
+          { required: true, message: '请输入状态' },
+        ],
+      },
+      url: {
+        list: "/shoes/shoeGoodsSku/list",
+        add: '/shoes/shoeGoods/add',
+        edit: '/shoes/shoeGoods/edit',
+        queryById: '/shoes/shoeGoods/queryById'
+      },
+      categoryList:[],
+
+
+      count:0,
+
+      columns : [
+        {
+          title: '规格名称',
+          dataIndex: 'skuTitle',
+          scopedSlots: { customRender: 'skuTitle' },
+        },
+        {
+          title: '规格图片',
+          dataIndex:'skuImage',
+          scopedSlots: {customRender: 'skuImage'},
+        },
+        {
+          title: '原价格',
+          dataIndex: 'originalPrice',
+          scopedSlots: { customRender: 'originalPrice' },
+
+        },
+        {
+          title: '优惠价格',
+          dataIndex: 'price',
+          scopedSlots: { customRender: 'price' },
+
+        },
+        {
+          title: '操作',
+          dataIndex: 'operation',
+          scopedSlots: { customRender: 'operation' },
+        },
+      ],
+
+      additionalList:[]
+
+
+    }
+  },
+  computed: {
+
+    formDisabled() {
+      return this.disabled
+    },
+  },
+  created() {
+    //备份model原始值
+    //  this.loadData(this.model.goodsId);
+    this.getCategoryList();
+    this.getAdditonalList();
+    this.modelDefault = JSON.parse(JSON.stringify(this.model))
+
+  },
+  methods: {
+
+    // 是否可用提交规格
+    isSubmitSkuTable() {
+      let status = true
+      const dataSource = [... this.model.skuTable];
+      dataSource.forEach((item,idx) => {
+        if(!item.originalPrice || !item.price || !item.skuTitle || !item.skuImage ) {
+          this.$message.warning(`第${idx+1}个规格没有填写完整，无法保存`)
+          dataSource.length = 0
+          status = false
+        }
+      });
+      return status
+    },
+
+    exceptSkuId(value){
+      if(value.key !== null){
+        return value.key !== value
+      }
+    },
+
+    onCellChange(record, dataIndex, value) {
+
+      console.log(999,value);
+      const dataSource = [... this.model.skuTable];
+
+
+      const target = dataSource[record];
+      if (target) {
+        target[dataIndex] = value;
+        this.model.skuTable = dataSource;
+      }
+    },
+    onDelete(index,record) {
+      console.log(444,index,record);
+      /**
+       * 删除存在有bug，待修复。
+       * @type {*[]}
+       */
+      this.model.skuTable.splice(index,1)
+      console.log(888,this.model.skuTable);
+
+    },
+    handleAdd() {
+
+      const dataSource  = this.model.skuTable;
+
+
+      let skuids=  [];
+      let count = 0;
+      let uuid = (Math.random() + new Date().getTime()).toString(32).slice(0,8);
+
+      const newData = {
+        skuId : count,
+        skuTitle: '',
+        price:'',
+        uuid
+      };
+      this.model.skuTable = [...dataSource, newData];
+      this.count = count + 1;
+      console.log(this.model.skuTable);
+
+    },
+
+
+    add() {
+      this.edit(this.modelDefault)
+    },
+    edit(record) {
+     // this.loadData(record.goodsId);
+     //
+      this.model = Object.assign({}, record)
+      this.model.skuTable = this.model.skuTable.map(item => {
+        return Object.assign({}, item,{uuid: (Math.random() + new Date().getTime()).toString(32).slice(0,8)})
+      })
+      this.visible = true
+    },
+    submitForm() {
+      if(!this.isSubmitSkuTable()) {
+        return
+      }
+      const that = this
+      console.log(this.model)
+      // 触发表单验证
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          that.confirmLoading = true
+          let httpurl = ''
+          let method = ''
+          if (!this.model.goodsId) {
+            httpurl += this.url.add
+            method = 'post'
+          } else {
+            httpurl += this.url.edit
+            method = 'put'
+          }
+          httpAction(httpurl, this.model, method).then((res) => {
+            if (res.success) {
+              that.$message.success(res.message)
+              that.$emit('ok')
+            } else {
+              that.$message.warning(res.message)
+            }
+          }).finally(() => {
+            that.confirmLoading = false
+          })
+        }
+
+      })
+
+    },
+    getCategoryList(){
+      httpAction("/shoes/shoeGoods/categoryList","","get").then((res)=>{
+        if(res){
+          this.categoryList = res.result;
+        }
+      })
+    },
+    getAdditonalList(){
+      httpAction("/shoes/shoeAdditional/queryList","", "get").then((res)=>{
+        if(res){
+          console.log(res.result);
+          this.additionalList = res.result;
+
+        }
+      })
+    },
+
+  },
+}
+</script>
+
+<style lang="less">
+.ant-input-number-handler-wrap {
+    display: none
+}
+
+.editable-row-operations a {
+  margin-right: 8px;
+}
+
+.editable-cell {
+  position: relative;
+}
+
+.editable-cell-input-wrapper,
+.editable-cell-text-wrapper {
+  padding-right: 24px;
+}
+
+.editable-cell-text-wrapper {
+  padding: 5px 24px 5px 5px;
+  cursor: pointer;
+}
+
+.editable-cell-icon,
+.editable-cell-icon-check {
+  position: absolute;
+  right: 0;
+  width: 20px;
+  cursor: pointer;
+}
+
+.editable-cell-icon {
+  line-height: 18px;
+  display: none;
+}
+
+.editable-cell-icon-check {
+  line-height: 28px;
+}
+
+.editable-cell:hover .editable-cell-icon {
+  display: inline-block;
+}
+
+.editable-cell-icon:hover,
+.editable-cell-icon-check:hover {
+  color: #108ee9;
+}
+
+.editable-add-btn {
+  margin-bottom: 8px;
+}
+</style>
