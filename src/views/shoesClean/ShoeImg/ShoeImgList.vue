bb0830bc81cf3e8dfd3b15e8af1b7856ce718f31
yzm <1371924520@qq.com>
2023-06-15 10:29:19 +0800

    小程序图片管理

---
+++ src/views/shoesClean/ShoeImg/ShoeImgList.vue
@@ -0,0 +1,195 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline" @keyup.enter.native="searchQuery">
+        <a-row :gutter="24">
+        </a-row>
+      </a-form>
+    </div>
+    <!-- 查询区域-END -->
+
+    <!-- 操作按钮区域 -->
+    <!-- <div class="table-operator">
+      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+      <a-dropdown v-if="selectedRowKeys.length > 0">
+        <a-menu slot="overlay">
+          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+        </a-menu>
+        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+      </a-dropdown>
+    </div> -->
+
+    <!-- table区域-begin -->
+    <div>
+      <!-- <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+      </div> -->
+
+      <a-table
+        ref="table"
+        size="middle"
+        :scroll="{x:true}"
+        bordered
+        rowKey="bannerId"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        class="j-table-force-nowrap"
+        @change="handleTableChange">
+
+        <template slot="htmlSlot" slot-scope="text">
+          <div v-html="text"></div>
+        </template>
+        <template slot="imgSlot" slot-scope="text,record">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
+          <img @click="onImg(text)" v-else :src="getImgView(text)" :preview="record.id" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+        </template>
+        <template slot="fileSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
+          <a-button
+            v-else
+            :ghost="true"
+            type="primary"
+            icon="download"
+            size="small"
+            @click="downloadFile(text)">
+            下载
+          </a-button>
+        </template>
+
+        <span slot="action" slot-scope="text, record">
+          <a @click="handleEditImg(record)">修改</a>
+        </span>
+
+      </a-table>
+    </div>
+
+    <a-modal v-model="showImg" :footer="null">
+      <img :src="imgUrl" style="width: 100%;"/>
+    </a-modal>
+
+    <!-- 修改图片 -->
+    <a-modal v-model="showEditImg" :footer="null" :maskClosable="false" :closable="false">
+      <div class="center">
+        <j-image-upload v-model="form.image" ></j-image-upload>
+        <div class="foot">
+          <a-button @click="showEditImg = false">取消</a-button>
+          <a-button type="primary" style="margin-left: 30px;" @click="onSubmit" :loading="confirmLoading">保存</a-button>
+        </div>
+      </div>
+    </a-modal>
+  </a-card>
+</template>
+
+<script>
+  import { httpAction } from '@/api/manage'
+  import '@/assets/less/TableExpand.less'
+  import { mixinDevice } from '@/utils/mixin'
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+
+  export default {
+    name: 'ShoeBannerList',
+    mixins:[JeecgListMixin, mixinDevice],
+    components: {
+      
+    },
+    data () {
+      return {
+        description: '图片管理页面',
+        // 表头
+        columns: [
+          {
+            title:'模块',
+            align:"center",
+            dataIndex: 'bannerId'
+          },
+          {
+            title:'图片',
+            align:"center",
+            dataIndex: 'image',
+            scopedSlots: {customRender: 'imgSlot'}
+          },
+          {
+            title:'尺寸',
+            align:"center",
+            dataIndex: 'url'
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            align:"center",
+            fixed:"right",
+            width:147,
+            scopedSlots: { customRender: 'action' }
+          }
+        ],
+        url: {
+          list: "/shoes/shoeBanner/list",
+        },
+        imgUrl:'',
+        showImg:false,
+        showEditImg:false,
+        form:{
+          image:'',
+          id:''
+        },
+        confirmLoading:false
+      }
+    },
+    created() {
+    },
+    computed: {
+      importExcelUrl: function(){
+        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+      },
+    },
+    methods: {
+      onSubmit() {
+        if(!this.form.image) {
+          return this.$message.warning('请选择一张图片!');
+        }
+        this.confirmLoading = true
+        httpAction('',this.form,'post').then((res)=>{
+          if(res.success){
+            this.$message.success(res.message);
+            this.showEditImg = false
+            this.loadData(1);
+          }else{
+            this.$message.warning(res.message);
+          }
+        }).finally(() => {
+          this.confirmLoading = false;
+        })
+      },
+      onImg(url) {
+        this.imgUrl = url
+        this.showImg = true
+      },
+      handleEditImg(record) {
+        this.showEditImg = true
+        this.form.image = record.image
+        this.form.id = record.image
+      },
+      initDictConfig(){
+      },
+    }
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less';
+  .center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .foot {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 24px;
+  }
+</style>
