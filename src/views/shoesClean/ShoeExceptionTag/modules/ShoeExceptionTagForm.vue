d03dc6ec5a4fc0c3ff6a86f24f97d440b124870e
yzm <1371924520@qq.com>
2023-06-26 17:29:56 +0800

    选择图片

--- src/views/shoesClean/ShoeExceptionTag/modules/ShoeExceptionTagForm.vue
+++ src/views/shoesClean/ShoeExceptionTag/modules/ShoeExceptionTagForm.vue
@@ -21,19 +21,34 @@
 
           <a-col :span="24">
             <a-form-model-item label="订单图片" :labelCol="labelCol" :wrapperCol="wrapperCol">
-              <j-image-upload isMultiple  v-model="model.images" ></j-image-upload>
+              <div class="imgs">
+                <div class="imgs-box" v-for="(img,idx) in model.images" :key="idx">
+                  <img class="imgs-box-img" :src="img" alt="" />
+                  <div class="show" @click="piveImage(img)">
+                    <a-icon type="eye" style="color:#fff;font-size: 20px;" />
+                  </div>
+                </div>
+              </div>
             </a-form-model-item>
           </a-col>
           <a-col :span="24">
-            <a-form-model-item label="入库图片" :labelCol="labelCol" :wrapperCol="wrapperCol" help="请选择至少一张图片作为异常图片">
-              <j-image-upload isMultiple  v-model="model.factoryImages" ></j-image-upload>
+            <a-form-model-item prop="factoryImages" label="入库图片" :labelCol="labelCol" :wrapperCol="wrapperCol" help="请选择至少一张图片作为异常图片">
+              <div class="imgs">
+                <div class="imgs-box" v-for="(img,idx) in factoryImages" :key="idx">
+                  <img class="imgs-box-img" :src="img.url" alt="" />
+                  <div class="imgs-box-select" :class="{selected: img.select}" @click="onSelect(idx)"></div>
+                  <div class="show" @click="piveImage(img.url)">
+                    <a-icon type="eye" style="color:#fff;font-size: 20px;" />
+                  </div>
+                </div>
+              </div>
             </a-form-model-item>
           </a-col>
 
 
           <a-col :span="24">
             <a-form-model-item label="处理方式" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="dealType">
-              <a-radio-group v-model:value="model.dealType">
+              <a-radio-group v-model="model.dealType">
                 <a-radio value="1">转异常</a-radio>
                 <a-radio value="2">继续洗</a-radio>
               </a-radio-group>
@@ -48,6 +63,9 @@
         </a-row>
       </a-form-model>
     </j-form-container>
+    <a-modal :visible="ImgVisible" :footer="null" @cancel="ImgVisible = false" width="600px">
+      <img alt="example" style="width: 100%" :src="previewImage"/>
+  </a-modal>
   </a-spin>
 </template>
 
@@ -72,6 +90,7 @@
       return {
         date:{},
         model:{
+          factoryImages:[]
          },
         labelCol: {
           xs: { span: 24 },
@@ -87,6 +106,9 @@
            dealType: [
               { required: true, message: '请选择处理方式!'},
            ],
+           factoryImages: [
+              { required: true, message: '请选择至少一张图片作为异常图片!'},
+           ],
            note: [
               { required: true, message: '请输入!'},
            ],
@@ -95,7 +117,10 @@
           add: "/shoeExceptionTag/add",
           edit: "/shoeExceptionTag/edit",
           queryById: "/shoeExceptionTag/shoeExceptionTag/queryById"
-        }
+        },
+        factoryImages:[],
+        previewImage:'',
+        ImgVisible:false
       }
     },
     computed: {
@@ -107,7 +132,22 @@
        //备份model原始值
       this.modelDefault = JSON.parse(JSON.stringify(this.model));
     },
+    watch:{
+      factoryImages: {
+        handler(val,oldValue) {
+          this.model.factoryImages = val.filter(item => item.select).map(item => item.url)
+        },
+        deep:true
+      },
+    },
     methods: {
+      onSelect(idx) {
+        this.factoryImages[idx].select = !this.factoryImages[idx].select
+      },
+      piveImage(url) {
+        this.previewImage = url
+        this.ImgVisible = true
+      },
       add () {
         this.edit(this.modelDefault);
       },
@@ -124,7 +164,10 @@
         httpAction("/shoeExceptionTag/getImages?no="+record.no,null,"get").then((res)=>{
           if (res.success){
             this.model.images=res.result.orderList
-            this.model.factoryImages=res.result.factoryList
+            this.factoryImages=res.result.factoryList.map(url => ({
+              url,
+              select:false
+            }))
             console.log("111",this.model)
 
           }
@@ -162,3 +205,59 @@
     }
   }
 </script>
+
+<style lang="less" scoped>
+.imgs {
+  display: flex;
+  flex-wrap: wrap;
+  &-box {
+    margin-right: 8px;
+    position: relative;
+    .selected {
+      background: #1890ff;
+    }
+    &:hover {
+      .show {
+        display: flex;
+        
+      }
+      cursor: pointer;
+      
+    }
+    &-img {
+      width: 104px;
+      height: 104px;
+     
+    }
+    &-select {
+      width: 20px;
+      height: 20px;
+      position: absolute;
+      right: 0;
+      top: 0;
+      border: 1px solid #f5f5f5;
+      z-index: 10;
+      &::after {
+          content: '';
+          position: absolute;
+          left: -16px;
+          right: 0;
+          top: 0;
+          bottom: -16px;
+      }
+    }
+  }
+  .show {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 5;
+    background: rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    display: none;
+    top: 0;
+  }
+}
+</style>
