9a3d09b725fe29e89df88970a1bc0e550e5b4e15
USER-20220902BJ\Administrator <774401914@qq.com>
2023-06-27 14:48:16 +0800

    异常上报

--- src/views/shoesClean/ShoeExceptionTag/modules/showExceptionTagDetail.vue
+++ src/views/shoesClean/ShoeExceptionTag/modules/showExceptionTagDetail.vue
@@ -56,14 +56,23 @@
           </a-col>
         </a-row>
       </a-form-model>
+    <a-modal :visible="ImgVisible" :footer="null" @cancel="ImgVisible = false" width="600px">
+      <img alt="example" style="width: 100%" :src="previewImage"/>
     </a-modal>
+    </a-modal>
+
 </template>
 
 <script>
+  import {httpAction} from "@api/manage";
+
   export default {
     name: 'ShoeExceptionTagModal',
     data () {
       return {
+        factoryImages:[],
+        previewImage:'',
+        ImgVisible:false,
         title:'',
         width:800,
         visible: false,
@@ -77,18 +86,42 @@
           sm: { span: 16 },
         },
         model: {
-
+          factoryImages:[],
         }
       }
     },
+    watch:{
+      factoryImages: {
+        handler(val,oldValue) {
+          this.model.factoryImages = val.filter(item => item.select).map(item => item.url)
+        },
+        deep:true
+      },
+    },
     methods: {
+      onSelect(idx) {
+        this.factoryImages[idx].select = !this.factoryImages[idx].select
+      },
+      piveImage(url) {
+        this.previewImage = url
+        this.ImgVisible = true
+      },
       show (record) {
         this.model = Object.assign({
           images:[],
           factoryImages:[]
         }, record);
+        this.getImages(record);
         this.visible=true
-        console.log(8888,this.model);
+      },
+      getImages(record){
+        httpAction("/shoeExceptionTag/getImages?no="+record.no,null,"get").then((res)=>{
+          if (res.success){
+            this.model.images=res.result.orderList
+            this.model.factoryImages=res.result.factoryList
+
+          }
+        })
       },
       close () {
         this.$emit('close');
@@ -100,3 +133,59 @@
     }
   }
 </script>
+
+<style lang="less" scoped>
+.imgs {
+  display: flex;
+  flex-wrap: wrap;
+  &-box {
+    margin-right: 8px;
+    position: relative;
+    .selected {
+      background: #1890ff;
+    }
+    &:hover {
+      .show {
+        display: flex;
+
+      }
+      cursor: pointer;
+
+    }
+    &-img {
+      width: 104px;
+      height: 104px;
+
+    }
+    &-select {
+      width: 20px;
+      height: 20px;
+      position: absolute;
+      right: 0;
+      top: 0;
+      border: 1px solid #f5f5f5;
+      z-index: 10;
+      &::after {
+        content: '';
+        position: absolute;
+        left: -16px;
+        right: 0;
+        top: 0;
+        bottom: -16px;
+      }
+    }
+  }
+  .show {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 5;
+    background: rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    display: none;
+    top: 0;
+  }
+}
+</style>
